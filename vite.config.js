bf44b1f928984c85eac6b296623e1b673189623d
Subrata Das <codesubrata@gmail.com>
2024-07-20 09:14:39 +0530

    updating workflows code

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/flight-to-fly",
+  base: "/flight-to-fly/",
 });
